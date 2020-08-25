2e2f778da498f37d4d66060489b371a306599ab5
leylasimone <33478255+leylasimone@users.noreply.github.com>
2020-08-25 20:22:16 +1000

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -14,10 +14,10 @@ using UnityEngine.SceneManagement;
 /// This script must be used as the core Player script for managing the player character in the game.
 /// </summary>
 public class Player : MonoBehaviour
-{
-    private Vector3 _lastpos;
-    private bool _onLilypad = false;
-    private float _waterTimer = 0.0f;
+{    
+    private Vector3 _lastpos; //player and lilypad location
+    private bool _onLilypad = false; //if player is on lilypad or not
+    private float _waterTimer = 0.0f; //timer for lilypad glitch 
 
     public string playerName = ""; //The players name for the purpose of storing the high score
      
@@ -29,7 +29,7 @@ public class Player : MonoBehaviour
 
     private GameManager myGameManager; //A reference to the GameManager in the scene.
 
-    private float _furthestY = -6.4f;
+    private float _furthestY = -6.4f; //no infinite stepping forward glitch - references players furthest step for 10 points
 
     // Start is called before the first frame update
     void Start()
@@ -42,6 +42,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //a delay for a lilypad glitch
         _waterTimer -= Time.deltaTime;
         if (_waterTimer <= 0)
         {
@@ -53,6 +54,7 @@ public class Player : MonoBehaviour
             if (Input.GetKeyUp(KeyCode.UpArrow) && transform.position.y < myGameManager.levelConstraintTop)
             {
                 transform.Translate(new Vector2(0, 1f));
+                // 10 points per forward step
                 if (transform.position.y > _furthestY)
                 {
                     _furthestY = this.transform.position.y;
@@ -82,23 +84,16 @@ public class Player : MonoBehaviour
     {
         if (collision.gameObject.tag == "Collider" && _onLilypad == false)
         {
-            playerTotalLives -= 1;
-            Score.CurrentScore = 0;
-            Lives.TotalLives = playerTotalLives;
-            transform.position = new Vector2(0, -6.5f);
-            //playerIsAlive = false;
-            if (playerTotalLives <= 0)
+            playerTotalLives -= 1; //take away player life when collided
+            Score.CurrentScore = 0; //reset score when collided
+            Lives.TotalLives = playerTotalLives; //for Lives script to change 
+            transform.position = new Vector2(0, -6.5f); //moves player to the start and leaves the scene running
+            if (playerTotalLives <= 0) //when player loses all lives 
             {
-                Lives.TotalLives += playerTotalLives;
-                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                Lives.TotalLives += playerTotalLives; //changes the lives on the screen text
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name); //resets the scene/game
             }
-
-            //death ui
-            //score change
-            //Score goes down
-            //myScore += "Score: " + 10; //update string of collision
         }
-        //playerIsAlive = true;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -107,24 +102,24 @@ public class Player : MonoBehaviour
         if (playerIsAlive = true)
         {
             //Check for a match with the specified tag on any GameObject that collides with your GameObject
-
             if (collision.gameObject.name.Contains("lily pad"))
             {
-                _onLilypad = true;
-                _waterTimer = 1.0f;
-                _lastpos = this.transform.position;
+                _onLilypad = true; //player on the lilypad
+                _waterTimer = 1.0f; //delay the collision of the water - lilypad glitch i had 
+                _lastpos = this.transform.position; //moves player with the lilypad
             }
         }        
     }
 
     void OnTriggerExit2D(Collider2D collision)
     {
-      Debug.Log(collision.gameObject.name);
+      //Debug.Log(collision.gameObject.name); //for testing collision
       if (collision.gameObject.name.Contains("lily pad"))
       {
         if (this.transform.position == _lastpos)
         {
-            this.transform.position += new Vector3((collision.gameObject.GetComponent<LilyPad>().moveRight ? 1 : -1), 0, 0);
+            //move player with lilypad location x
+            this.transform.position += new Vector3((collision.gameObject.GetComponent<LilyPad>().moveRight ? 1 : -1), 0, 0); 
         }
         else
         {
